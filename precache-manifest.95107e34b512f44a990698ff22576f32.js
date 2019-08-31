84b84002d4ffa764d295f7ae5ab3c90ad262dade
vortesnail <1091331061@qq.com>
2019-08-31 15:51:00 +0800

    packge.json change

---
+++ precache-manifest.95107e34b512f44a990698ff22576f32.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11255c3f51b18f02a9afc69f2fdd96d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "31081c1db054b2a0f09c",
+    "url": "./static/css/main.b5e80726.chunk.css"
+  },
+  {
+    "revision": "f5ba0b422b7e0e208b18",
+    "url": "./static/js/2.dc81fc54.chunk.js"
+  },
+  {
+    "revision": "31081c1db054b2a0f09c",
+    "url": "./static/js/main.0721e24f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3fd96d908d4c11744a82c57bfdd1adb9",
+    "url": "./static/media/1G-1.3fd96d90.jpg"
+  },
+  {
+    "revision": "a701463921c2ff385fabd8008e50198b",
+    "url": "./static/media/1G-2.a7014639.jpg"
+  },
+  {
+    "revision": "8ab542f8f0cae547f3e83cee5d5e3d8f",
+    "url": "./static/media/1G-3.8ab542f8.jpg"
+  },
+  {
+    "revision": "de180caf49b6b75dccd26bdf01a796d8",
+    "url": "./static/media/1G-4.de180caf.jpeg"
+  },
+  {
+    "revision": "0c1579d3036c6ac2c6a2a7e0f1b73e20",
+    "url": "./static/media/2G-1.0c1579d3.jpg"
+  },
+  {
+    "revision": "92966198dddd0ed33228e5c8f4d515c3",
+    "url": "./static/media/2G-2.92966198.jpg"
+  },
+  {
+    "revision": "d4dc6d9849bb1b35b7332780be8ce819",
+    "url": "./static/media/2G-3.d4dc6d98.jpg"
+  },
+  {
+    "revision": "93e9e251c82a933f27e80902ea75444d",
+    "url": "./static/media/2G-4.93e9e251.jpg"
+  },
+  {
+    "revision": "6a497aba26fb96585ae51a3e67c5bf0a",
+    "url": "./static/media/4G-1.6a497aba.jpg"
+  },
+  {
+    "revision": "06ec5bbdddd6ec52b12462a23bc942ec",
+    "url": "./static/media/4G-2.06ec5bbd.jpg"
+  },
+  {
+    "revision": "4455eb5911d51857b19032971041ca2f",
+    "url": "./static/media/4G-3.4455eb59.jpg"
+  },
+  {
+    "revision": "8e751cb1e86c3acd51a1854f0a0a2ee3",
+    "url": "./static/media/4G-4.8e751cb1.jpg"
+  },
+  {
+    "revision": "45afdb399e55a9398fcc033d4a13add5",
+    "url": "./static/media/4G-5.45afdb39.png"
+  },
+  {
+    "revision": "6f10bc112db6e3d7cfaa775f87f7f88e",
+    "url": "./static/media/4G-6.6f10bc11.png"
+  },
+  {
+    "revision": "b71e787f7b4cefb4b670be25c2e4135f",
+    "url": "./static/media/4G-7.b71e787f.jpg"
+  },
+  {
+    "revision": "ccb16228d611d8a2407620b135f90a92",
+    "url": "./static/media/4G-8.ccb16228.jpg"
+  },
+  {
+    "revision": "2031631433a2c8b4e60e448f7b034f1b",
+    "url": "./static/media/4G-9.20316314.png"
+  },
+  {
+    "revision": "1f9c8c8949318e6911bf4bee065001da",
+    "url": "./static/media/5G.1f9c8c89.jpg"
+  },
+  {
+    "revision": "adb0c4d4e34e8c750b5eb6dca802d8c4",
+    "url": "./static/media/N.adb0c4d4.png"
+  },
+  {
+    "revision": "fe3d2416ab0acb44b30a5c472893e2c4",
+    "url": "./static/media/deathStranding.fe3d2416.mp4"
+  },
+  {
+    "revision": "a2c6af7d34f94d410ae6239839282fea",
+    "url": "./static/media/moon.a2c6af7d.gif"
+  },
+  {
+    "revision": "79dc7c1463ec4a3b0a5f45dc897e70e3",
+    "url": "./static/media/satallite.79dc7c14.gif"
+  },
+  {
+    "revision": "94b0fab961cee5fa462c9084fb130dd3",
+    "url": "./static/media/shouji-1.94b0fab9.jpeg"
+  },
+  {
+    "revision": "b178ec720ed1dd9f68a5ac6bc8b174b3",
+    "url": "./static/media/shouji-2.b178ec72.jpeg"
+  },
+  {
+    "revision": "baa912593335f6610bf4111a3f4dff88",
+    "url": "./static/media/starSky.baa91259.png"
+  },
+  {
+    "revision": "e3e45fed2189ad184c57c85d49f8dedc",
+    "url": "./static/media/what-is-5g.e3e45fed.png"
+  }
+]);
